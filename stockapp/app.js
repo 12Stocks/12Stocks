4c8ee0fcb4f7458a550aeada88e07d0140e0b1da
lkt9899 <lkt9899@naver.com>
2022-11-16 16:45:49 +0900

    [ADD] crawling test

--- stockapp/app.js
+++ stockapp/app.js
@@ -46,10 +46,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-const port = process.env.APP_PORT || 3000;
+// const port = process.env.APP_PORT || 3000;
 
-app.listen(port, function () {
-  console.log('Example app listening on port ' + port);
-});
+// app.listen(port, function () {
+//   console.log('Example app listening on port ' + port);
+// });
 
 module.exports = app;

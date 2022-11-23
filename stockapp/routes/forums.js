0e8135520a95b58a7c3c4d094215af1992ff4c6f
can13any <mnvyiions@gmail.com>
2022-11-24 03:51:26 +0900

    게시판 페이지 추가

--- stockapp/routes/forums.js
+++ stockapp/routes/forums.js
@@ -2,7 +2,22 @@ var express = require('express')
 var router = express.Router();
 
 router.get('/', function (req, res) {
-    res.render('forums');
+    res.render('forums/index');
+});
+router.get('/show', function (req, res) {
+    res.render('forums/show');
+});
+router.get('/new', function (req, res) {
+    res.render('forums/new');
+});
+router.post('/new', function (req, res) {
+    // res.render('forums/new');
+});
+router.get('/edit', function (req, res) {
+    res.render('forums/edit');
+});
+router.post('/edit', function (req, res) {
+    // res.render('forums/edit');
 });
 
 module.exports = router;

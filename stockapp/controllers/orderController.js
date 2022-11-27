bd15c41ee680d79a62d9f093b0594ce3ef25edfe
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2022-11-28 00:46:10 +0900

    주문 넣기

---
+++ stockapp/controllers/orderController.js
@@ -0,0 +1,29 @@
+const db = require('../config/DB');
+
+module.exports = {
+    GetTickSize : function getTickSize(price) {
+        var p = parseInt(price.replace(/,/g, ''));
+        if (p.length < 4) return 1;
+        if (1000 <= p && p < 5000) return 5;
+        if (5000 <= p && p < 10000) return 10;
+        if (10000 <= p && p < 50000) return 50;
+        if (50000 <= p && p < 100000) return 100;
+        if (100000 <= p && p < 500000) return 500;
+        if (500000 <= p) return 1000;
+    },
+
+    // buy : 0, sell : 1
+    Order : function(userId, stock_code, quantity, buysell, price, cb) {
+        var sql = "INSERT INTO offers (trader_id, stock_code, quantity, buysell, price, DT)"
+            + " VALUES (?, ?, ?, ?, ?, CURRENT_TIMESTAMP);";
+        db.query(sql, [userId, stock_code, quantity, buysell, price], function (err) {
+            if (err) throw err;
+            console.log("주문 완료");
+            cb();
+        });
+    },
+
+    GetOrderList : function() {
+       
+    }
+}
--- stockapp/routes/order.js
+++ stockapp/routes/order.js
@@ -2,14 +2,45 @@ var express = require('express');
 var router = express.Router();
 var auth = require('../lib/auth');
 var crawling = require('../crawling/crawling');
+var orderController = require("../controllers/orderController");
 
 router.get('/:item_code', function (req, res) {
     crawling.ItemPrice(req, res, function () {
+        
+        var tickSize = orderController.GetTickSize(req.itemInfo.now);
+        var datas = { 
+                itemInfo: req.itemInfo, 
+                tickSize: tickSize,
+            };
+
+        // TODO : refactor all the other codes like this
         if (auth.isOwner(req, res)) {
-            res.render('order', { userId: req.user.user_id, itemInfo: req.itemInfo });
-        } else {
-            res.render('order', { itemInfo: req.itemInfo });
+            datas.userId = req.user.user_id;
+            res.render('order', datas);
+        } 
+        else {
+            res.redirect('/auth/loginRequired');
         }
+    });
+});
+
+// buy : 0
+router.post('/:item_code/buy_process', function (req, res) {
+    var post = req.body;
+    var buy_quantity = post.bq;
+    var buy_price = post.bp;
+    orderController.Order(req.user.user_id, req.params.item_code, buy_quantity, 0, buy_price, function() {
+        res.redirect('/order/' + req.params.item_code);
+    });
+});
+
+// sell : 1
+router.post('/:item_code/sell_process', function (req, res) {
+    var post = req.body;
+    var sell_quantity = post.sq;
+    var sell_price = post.sp;
+    orderController.Order(req.user.user_id, req.params.item_code, sell_quantity, 1, sell_price, function () {
+        res.redirect('/order/' + req.params.item_code);
     });
 });
 

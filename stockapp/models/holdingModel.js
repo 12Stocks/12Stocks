06d4bd2879eb8e895f937f0ba4cc7b7aa2bb2961
lkt9899 <lkt9899@naver.com>
2022-12-10 23:13:48 +0900

    [ADD] holding interface

--- stockapp/models/holdingModel.js
+++ stockapp/models/holdingModel.js
@@ -2,7 +2,7 @@ const db = require("../config/DB");
 
 module.exports = {
     getHolding : function(user_id, cb) {
-        var sql = "SELECT * FROM holding WHERE user_id = ?;";
+        var sql = "select H.*, S.stock_name from holding H, stocks S where user_id = ? and H.stock_code = S.stock_code;";
 
         db.query(sql, user_id, (err, rows, fields) => {
             if(err) throw err;
